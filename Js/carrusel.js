8726fbfec1b9b422357116662a4f8063dafaf33c
lisandro cordoba <lisandrocba_7@hotmail.com>
2021-11-29 19:19:59 -0300

    cambios 29.11

--- Js/carrusel.js
+++ Js/carrusel.js
@@ -1,4 +1,4 @@
-let fotos = ["img/prueba1.jpg","img/prueba2.jpg","img/prueba3.jpg"]
+let fotos = ["img/galeria/prueba1.jpg","img/galeria/prueba2.jpg","img/galeria/prueba3.jpg"]
 
 flechIzq = document.getElementById("flechaIzq");
 flechDer = document.getElementById("flechaDer");
@@ -7,41 +7,41 @@ let contador = 0;
 flechIzq.addEventListener("click", function(){
     if(contador == 0){
         contador ++;
-        document.getElementById("imag1").src = "img/prueba2.jpg";
-        document.getElementById("imag2").src = "img/prueba3.jpg";
-        document.getElementById("imag3").src = "img/prueba1.jpg";
+        document.getElementById("imag1").src = "img/galeria/prueba2.jpg";
+        document.getElementById("imag2").src = "img/galeria/prueba3.jpg";
+        document.getElementById("imag3").src = "img/galeria/prueba1.jpg";
     }
     else if (contador == 1){
         contador ++;
-        document.getElementById("imag1").src = `img/prueba${3}.jpg`;
-        document.getElementById("imag2").src = `img/prueba${1}.jpg`;
-        document.getElementById("imag3").src = `img/prueba${2}.jpg`;
+        document.getElementById("imag1").src = `img/galeria/prueba${3}.jpg`;
+        document.getElementById("imag2").src = `img/galeria/prueba${1}.jpg`;
+        document.getElementById("imag3").src = `img/galeria/prueba${2}.jpg`;
     }
     else{
         contador = 0;
-        document.getElementById("imag1").src = `img/prueba${1}.jpg`;
-        document.getElementById("imag2").src = `img/prueba${2}.jpg`;
-        document.getElementById("imag3").src = `img/prueba${3}.jpg`;
+        document.getElementById("imag1").src = `img/galeria/prueba${1}.jpg`;
+        document.getElementById("imag2").src = `img/galeria/prueba${2}.jpg`;
+        document.getElementById("imag3").src = `img/galeria/prueba${3}.jpg`;
     }   
 });
 
 flechDer.addEventListener("click", function(){
        if(contador == 0){
         contador = 2;
-        document.getElementById("imag1").src = `img/prueba${3}.jpg`;
-        document.getElementById("imag2").src = `img/prueba${1}.jpg`;
-        document.getElementById("imag3").src = `img/prueba${2}.jpg`;
+        document.getElementById("imag1").src = `img/galeria/prueba${3}.jpg`;
+        document.getElementById("imag2").src = `img/galeria/prueba${1}.jpg`;
+        document.getElementById("imag3").src = `img/galeria/prueba${2}.jpg`;
     }
     else if (contador == 2){
         contador --;
-        document.getElementById("imag1").src = "img/prueba2.jpg";
-        document.getElementById("imag2").src = "img/prueba3.jpg";
-        document.getElementById("imag3").src = "img/prueba1.jpg";
+        document.getElementById("imag1").src = "img/galeria/prueba2.jpg";
+        document.getElementById("imag2").src = "img/galeria/prueba3.jpg";
+        document.getElementById("imag3").src = "img/galeria/prueba1.jpg";
     }
     else{
         contador --;
-        document.getElementById("imag1").src = `img/prueba${1}.jpg`;
-        document.getElementById("imag2").src = `img/prueba${2}.jpg`;
-        document.getElementById("imag3").src = `img/prueba${3}.jpg`;
+        document.getElementById("imag1").src = `img/galeria/prueba${1}.jpg`;
+        document.getElementById("imag2").src = `img/galeria/prueba${2}.jpg`;
+        document.getElementById("imag3").src = `img/galeria/prueba${3}.jpg`;
     }   
 });
